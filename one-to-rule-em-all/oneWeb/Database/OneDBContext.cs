2f4400dcb69fd923658b052df00e3ce2e72eada8
rot4ry <wojtekw233@gmail.com>
2022-09-23 16:38:13 +0200

    A: Database/ and DB context.

---
+++ one-to-rule-em-all/oneWeb/Database/OneDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using oneWeb.Models;
+
+namespace oneWeb.Database {
+  public class OneDBContext : DbContext {
+    public OneDBContext (DbContextOptions<OneDBContext> options) : base(options) {
+    }
+
+    // TODO: Add models
+    //public DbSet<HotelModel> Hotels { get; set; }
+    //public DbSet<UserModel> Users { get; set; }
+  }
+}
