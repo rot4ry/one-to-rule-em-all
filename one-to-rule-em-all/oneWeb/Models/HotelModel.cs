65498183f895f4b64c43dad9b9fb672ed531ba85
rot4ry <wojtekw233@gmail.com>
2022-09-23 17:33:36 +0200

    U: HotelModel properties.

--- one-to-rule-em-all/oneWeb/Models/HotelModel.cs
+++ one-to-rule-em-all/oneWeb/Models/HotelModel.cs
@@ -4,14 +4,14 @@ namespace oneWeb.Models {
   public class HotelModel {
     [Key]
     public int Id { get; set; }
-    
+
     [Required]
     [MinLength(5)]
     public string HotelName { get; set; }
-    
+
     [Required]
     public string Country { get; set; }
-    
+
     [Required]
     public string City { get; set; }
 
