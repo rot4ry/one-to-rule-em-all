e6fbf2e368cf5375d85bdd5cbb63fb9f2fa0be48
rot4ry <wojtekw233@gmail.com>
2022-09-24 13:06:47 +0200

    A: HotelController.

---
+++ one-to-rule-em-all/oneWeb/Controllers/HotelController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using oneWeb.Database;
+
+namespace oneWeb.Controllers {
+  public class HotelController: Controller {
+    private readonly OneDBContext _dbContext;
+
+    public HotelController (OneDBContext dbContext) {
+      _dbContext = dbContext;
+    }
+
+    // GET: Hotel list
+    public async Task<IActionResult> Index () {
+      return View(await _dbContext.Hotels.ToListAsync());
+    }
+
+    // GET: Create hotel
+    public IActionResult Create () {
+      return View();
+    }
+
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public async Task<IActionResult> Create ([Bind("Id,Name")] HotelModel hotel) {
+    //  if (ModelState.IsValid) {
+    //    _context.Hotels.Add(category);
+    //    await _context.SaveChangesAsync();
+    //    return RedirectToAction(nameof(Index));
+    //  }
+    //  return View(hotel);
+    //}
+  }
+}
